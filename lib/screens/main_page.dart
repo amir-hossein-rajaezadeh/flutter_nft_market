71f16b02344b42290d2cff9594a8c0c09ee06e99
AmirHossein <73388470+amir-hossein-rajaezadeh@users.noreply.github.com>
2024-05-25 15:24:20 +0330

    first init

---
+++ lib/screens/main_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: Text("MainPage"),
+          )
+        ],
+      ),
+    );
+  }
+}
