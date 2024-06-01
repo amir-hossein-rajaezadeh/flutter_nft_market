1f074851dbcd9cae761bfffa484dd912e6febc2e
AmirHossein <73388470+amir-hossein-rajaezadeh@users.noreply.github.com>
2024-06-01 13:55:20 +0330

    Added bottom nav bar

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,12 +1,9 @@
 import 'dart:ui';
-
 import 'package:expandable_page_view/expandable_page_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_nft_market/utils/my_colors.dart';
+import 'package:flutter_nft_market/widgets/bottom_nav_bar.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -25,145 +22,180 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MyColors.background,
-      body: Column(
+      body: Stack(
+        alignment: Alignment.bottomCenter,
         children: [
-          Container(
-            alignment: Alignment.center,
-            margin: const EdgeInsets.only(top: 60),
-            child: const Text(
-              "NFT Market Place",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 32,
-                  fontWeight: FontWeight.w600),
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 32),
-            child: ExpandablePageView.builder(
-              animationDuration: const Duration(milliseconds: 300),
-              controller: _pageController,
-              onPageChanged: (index) => setState(() {
-                selectedIndex = index;
-              }),
-              itemCount: 3,
-              itemBuilder: (context, index) {
-                return AnimatedContainer(
-                  width: 280,
-                  height: selectedIndex == index ? 220 : 200,
-                  duration: const Duration(milliseconds: 270),
-                  margin: const EdgeInsets.only(right: 10),
-                  child: Container(
-                    width: 280,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(22),
-                      child: Image.asset(
-                        "assets/images/background.png",
-                        fit: BoxFit.cover,
+          ListView(
+            children: [
+              Container(
+                alignment: Alignment.center,
+                margin: const EdgeInsets.only(top: 20),
+                child: const Text(
+                  "NFT Market Place",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 32),
+                child: ExpandablePageView.builder(
+                  animationDuration: const Duration(milliseconds: 300),
+                  controller: _pageController,
+                  onPageChanged: (index) => setState(() {
+                    selectedIndex = index;
+                  }),
+                  itemCount: 3,
+                  itemBuilder: (context, index) {
+                    return AnimatedContainer(
+                      width: 280,
+                      height: selectedIndex == index ? 220 : 200,
+                      duration: const Duration(milliseconds: 270),
+                      margin: const EdgeInsets.only(right: 10),
+                      child: Container(
+                        width: 280,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(22),
+                          child: Image.asset(
+                            "assets/images/background.png",
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
+                    );
+                  },
+                ),
+              ),
+              _buildListTitleWidget("Trending collections"),
+              _buildListViewWidget(),
+              _buildListTitleWidget("Top seller"),
+              _buildListViewWidget(),
+            ],
+          ),
+          const BottomNaviagationBarWidget(),
+          Positioned(
+            bottom: 55,
+            child: GestureDetector(
+              onTap: () {},
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    alignment: Alignment.topCenter,
+                    height: 60,
+                    child: Image.asset(
+                      "assets/images/polygon.png",
                     ),
                   ),
-                );
-              },
+                  const Icon(
+                    Icons.add,
+                    color: Colors.white,
+                    size: 40,
+                  )
+                ],
+              ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 30, top: 25),
-            alignment: Alignment.topLeft,
-            child: const Text(
-              "Trending collections",
-              style: TextStyle(
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 20),
-            height: 213,
-            child: ListView.builder(
-              itemCount: 5,
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                return ClipRRect(
-                  borderRadius: BorderRadius.circular(30),
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 16, sigmaY: 16),
-                    child: Container(
-                      margin: const EdgeInsets.only(left: 10),
-                      width: 180,
-                      decoration: BoxDecoration(
-                        color: Colors.transparent.withOpacity(0.11),
-                        // border: Border.all(
-                        //   color: const Color(0xFF24224B),
-                        // ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Container _buildListViewWidget() {
+    return Container(
+      margin: const EdgeInsets.only(top: 6),
+      height: 213,
+      child: ListView.builder(
+        itemCount: 10,
+        padding: const EdgeInsets.only(left: 0),
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 16, sigmaY: 16),
+              child: Container(
+                margin: const EdgeInsets.only(left: 10),
+                width: 180,
+                decoration: BoxDecoration(
+                  color: Colors.transparent.withOpacity(0.11),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 9),
+                      width: 160,
+                      height: 160,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(22),
+                        child: Image.asset(
+                          "assets/images/background.png",
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                      child: Column(
+                    ),
+                    Container(
+                      margin:
+                          const EdgeInsets.only(top: 10, right: 20, left: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Container(
-                            margin: const EdgeInsets.only(top: 9),
-                            width: 160,
-                            height: 160,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(22),
-                              child: Image.asset(
-                                "assets/images/background.png",
-                                fit: BoxFit.cover,
-                              ),
-                            ),
+                          const Text(
+                            "Test",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 12,
+                                color: Colors.white),
                           ),
                           Container(
-                            margin: const EdgeInsets.only(
-                                top: 10, right: 20, left: 20),
                             child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                const Text(
-                                  "Test",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w700,
-                                      fontSize: 12,
-                                      color: Colors.white),
+                                Container(
+                                  margin: const EdgeInsets.only(right: 2),
+                                  child: const Icon(
+                                    CupertinoIcons.heart_fill,
+                                    color: Colors.red,
+                                    size: 18,
+                                  ),
                                 ),
                                 Container(
-                                  child: Row(
-                                    children: [
-                                      Container(
-                                        margin: const EdgeInsets.only(right: 2),
-                                        child: const Icon(
-                                          CupertinoIcons.heart_fill,
-                                          color: Colors.red,
-                                          size: 18,
-                                        ),
-                                      ),
-                                      Container(
-                                        child: Text(
-                                          "100",
-                                          style: TextStyle(
-                                              fontSize: 10,
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.grey.shade500),
-                                        ),
-                                      ),
-                                    ],
+                                  child: Text(
+                                    "100",
+                                    style: TextStyle(
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.grey.shade500),
                                   ),
                                 ),
                               ],
                             ),
-                          )
+                          ),
                         ],
                       ),
-                    ),
-                  ),
-                );
-              },
+                    )
+                  ],
+                ),
+              ),
             ),
-          )
-        ],
+          );
+        },
+      ),
+    );
+  }
+
+  Container _buildListTitleWidget(String titleValue) {
+    return Container(
+      margin: const EdgeInsets.only(left: 20, top: 25),
+      alignment: Alignment.topLeft,
+      child: Text(
+        titleValue,
+        style: const TextStyle(
+            fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
       ),
     );
   }
---
+++ lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,56 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class BottomNaviagationBarWidget extends StatelessWidget {
+  const BottomNaviagationBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(right: 4, left: 4, bottom: 6),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(50),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 40, sigmaY: 40),
+          child: SizedBox(
+            height: 80,
+            child: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  height: 80,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      buildNavBarIcon(CupertinoIcons.home, false),
+                      buildNavBarIcon(CupertinoIcons.search, true),
+                      buildNavBarIcon(CupertinoIcons.chart_bar, false),
+                      buildNavBarIcon(CupertinoIcons.person, false),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildNavBarIcon(IconData iconWidget, bool hasMarginRight) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 6, right: hasMarginRight ? 50 : 0),
+      child: Icon(
+        iconWidget,
+        color: Colors.white,
+        size: 28,
+      ),
+    );
+  }
+}
